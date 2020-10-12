a1e3e2e84965d6543ec1545d115d5b212490b46f
Khimer Rokovoy <castellio@bk.ru>
2020-10-12 20:23:27 +0800

    Initial Commit

---
+++ Circle Bar Progress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Circle Bar Progress
+//
+//  Created by Кирилл Романенко on 12/10/2020.
+//  Copyright © 2020 Кирилл. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
